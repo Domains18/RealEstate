d8dade46581e563388fa78812685aab143fc90cf
Gibsons Gibson <gibsonsgibson88@gmail.com>
2022-06-17 15:07:49 +0300

    commit on main

--- Main.js
+++ Main.js
@@ -1,12 +1,28 @@
+alert('Welcome to Keevadis Agencies');
+//changing navigation bar styles on scrolls
 
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0)
+})
 
+//show answer
 
+const faqs = document.querySelectorAll('.faq');
 
-
-
-
-
-
+faqs.forEach(faq =>{
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+//change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className=== 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } 
+        else{
+            icon.className = "uil uil-plus";
+        }
+    })
+})
 
 
 
